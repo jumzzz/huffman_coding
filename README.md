39079fea517cedbaa598ed8d84e45bd8b5cee0e0
jums <Jummel_Sasil@trendmicro.com>
2022-12-29 14:51:57 +0800

    add test on maximum difference

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Implementation of Huffman Coding in Rust
 - Given a Vector of Arbitrary Characters with corresponding Probabilities, generate the appropriate Huffman Code
 - Testing against Source Coding Theorem
 - Testing against Uniform Distribution of 8 Samples
+- Testing on the maximum difference between Expected Length L(C,X) and Entropy H(X) 
 
 ## Test
 
@@ -20,7 +21,7 @@ Here are the following test case
 
 1. Uniform Distribution with 8 Samples
 2. Shannon Source Coding Theorem
-
+3. Testing on the maximum difference between Expected Length `L(C,X)` and Entropy `H(X)`. 
 
 ## Reference
 -  *Information Theory, Inference and Learning Algorithms (2005) by McKay D.J.C. Chapter 05 - Symbol Codes*
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -13,7 +13,7 @@ mod tests {
             "e".to_string(), 
             "f".to_string(), 
             "g".to_string(), 
-            "h".to_string(), 
+            "h".to_string(),
         ];
     
         let probs = vec![
@@ -108,5 +108,72 @@ mod tests {
 
 
     }
+
+
+    /// # Maximum Difference between expected length 
+    /// The difference between the expected length *L* and the entropy
+    /// H can be no bigger than max(p_max, 0.086). (Gallager, 1978)
+    #[test]
+    fn test_max_limit() {
+        // (Gallager, 1978). The Difference
+        let probs: Vec<f32> = [1.0 / 11.0 ; 11].to_vec();
+        
+        let alphabets = vec![
+            "a".to_string(),
+            "b".to_string(), 
+            "c".to_string(), 
+            "d".to_string(), 
+            "e".to_string(),
+            "f".to_string(),
+            "g".to_string(),
+            "h".to_string(),
+            "i".to_string(),
+            "j".to_string(),
+            "k".to_string(), 
+        ];
+        
+
+        let huffman_gen = HuffmanGenerator::build(&alphabets, &probs);
+        huffman_gen.propagate_codes();
+
+        let mut code_lengths = Vec::new();
+        let mut probs = Vec::new();
+
+        println!("\nTest #3: Testing max limit.");
+        for (k,v) in huffman_gen.code_map.borrow().iter() {
+            println!("key = {}, code = {}", &k, &v.code);
+
+            let code_length = v.code.len() as f32;
+            code_lengths.push(code_length);
+            probs.push(v.prob);
+        }
+
+        let log_probs = probs.iter().map(|v| -v.log2());
+        
+        let entropy: f32 = probs.iter()
+                                .zip(log_probs)
+                                .map(|(a,b)| a * b)
+                                .sum();
+        
+        let expected_length: f32 = probs.iter()
+                                        .zip(code_lengths.iter())
+                                        .map(|(a,b)| a * b)
+                                        .sum();
+
+        let max_diff = expected_length - entropy;
+        let epsilon = (max_diff - 0.086).abs(); 
+        println!("\nH(X) = {}", entropy);
+        println!("L(C,X) = {}", expected_length);
+        println!("H(X) + 1 = {}", entropy + 1_f32);
+
+        println!("L(C,X) - H(X) = {}", max_diff);
+        println!("epsilon = {} \n", (max_diff - 0.086).abs());
+
+        assert!(epsilon < 0.0001);
+        
+
+        // assert_ep;
+    }
+
 
 }
